a8b5deaa0114472c07d8d2ec3c65602456762705
Marina Navarro <Marina Navarro@DESKTOP-G2S49F0>
2018-03-19 20:29:10 -0600

    Datos personales

---
+++ src/datospersonales/DatosPersonales.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datospersonales;
+
+public class DatosPersonales {
+
+    /**
+     * Programa que despliega datos personales
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombres: Daniel de Jesus");
+        System.out.println("Apellidos: Navarro Dominguez");
+        System.out.println("Edad: 21");
+        System.out.println("Fecha de nacimineto: 9/12/1996");
+        
+    }
+    
+}
