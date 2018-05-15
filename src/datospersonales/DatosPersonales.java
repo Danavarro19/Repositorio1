373a1bb2a406a188a9c82b2b120e82bc5d9a25e7
Danavarro19 <DanielNavarro09@gmail.com>
2018-05-14 22:06:50 -0600

    Espero poder ver esto en netbeans

--- src/datospersonales/DatosPersonales.java
+++ src/datospersonales/DatosPersonales.java
@@ -12,7 +12,7 @@ public class DatosPersonales {
      * @param args 
      */
     public static void main(String[] args) {
-        System.out.println("Pene");
+        System.out.println("Cool");
     }
     
 }
